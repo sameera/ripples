d01991ebdcc6c794b9325f802815d5f30995fdfe
sameera <www.sameera@gmail.com>
2026-02-07 07:24:56 -0500

    feat: implement #12 - TASK-7.05 Unit  E2E Tests

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm exec nx run ripple:preview',
+    command: 'pnpm exec nx run web:preview',
     url: 'http://localhost:4200',
     reuseExistingServer: true,
     cwd: workspaceRoot
