b9389a0c943c75d1ee9818d8731ac76be7e862e2
sameera <www.sameera@gmail.com>
2026-08-29 06:57:51 -0400

    chore: clean up for v2

--- apps/web-e2e/src/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("has title", async ({ page }) => {
-    await page.goto("/");
-
-    // App redirects to /stream which shows "Daily Stream" heading
-    await expect(page.locator("h2")).toContainText("Daily Stream");
-});
